e84803a1632e67dc4dc146d4c6cb69817408d317
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-12-15 12:58:58 -0600

    constants - assignments on the same line - long numbers with underscores - floats - exponents

--- Intro.py
+++ Intro.py
@@ -47,4 +47,43 @@ print(message)
 message = 'One of Python\'s strengths is its diverse community.'
 print(message)
 
+# 3 ** 3 = 27 --> exponents 3 ** 2 = 9
+print(3 ** 2)
+
+# order of operations
+# 14
+print(2 + 3 * 4)
+
+# 20
+print((2 + 3) * 4)
+
+# floats
+print(0.1 + 0.1)
+
+# 0.30000000000000004
+print(0.2 + 0.1)
+# 0.30000000000000004
+print(3 * 0.1)
+
+# When you divide any two numbers, even if they are integers that result in a
+#   whole number, you'll always get a float
+# 4/2 = 2.0
+print(4/2)
+
+# When writing long numbers, you can group digits with underscores
+long_number = 14_000_000_000
+
+# 14000000000
+print(long_number)
+
+# you can assign values to more than one variable using one line
+x, y, z = 1, 2, 3
+# y = 2
+print(y)
+
+# python does not have built-in constant types but developers use all capital letters
+#   to indicate a constant not to be changed
+
+MAX_CONNECTIONS = 5000
+
 
